a6da1626fe796c333d2ea00ea2999a9a6ba7c896
Juanpa1130 <pabloguzman121989@gmail.com>
2024-09-26 19:21:37 -0600

    modelos de angel
    los modelos que creo angel

---
+++ FacturacionLabco/FacturacionLabco/Models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacturacionLabco.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id_Producto { get; set; }
+
+        [Required(ErrorMessage = "Nombre del Producto es obligatorio")]
+        public string Descripcion_Producto { get; set; }
+
+
+        [Required(ErrorMessage = "Precio del Producto es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "el Precio tiene que ser mayor a cero")]
+        public float Precio_Producto { get; set; }
+
+
+        [Required(ErrorMessage = "Estado del Producto obligatorio")]
+        public Boolean Estado_Producto { get; set; }
+
+
+        [Required(ErrorMessage = "Stock del Producto es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "el Stock tiene que ser mayor a cero")]
+        public float Stock_Producto { get; set; }
+    }
+}
---
+++ FacturacionLabco/FacturacionLabco/Models/Servicio.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FacturacionLabco.Models
+{
+    public class Servicio
+    {
+        [Key]
+        public int Id_Servicio { get; set; }
+
+        [Required(ErrorMessage = "Nombre del Servicio es obligatorio")]
+        public string Descripcion_Servicio { get; set; }
+
+
+        [Required(ErrorMessage = "Precio del Servicio es obligatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "el Precio tiene que ser mayor a cero")]
+        public float Precio_Servicio { get; set; }
+
+
+        [Required(ErrorMessage = "Estado del Servicio obligatorio")]
+        public Boolean Estado_Producto { get; set; }
+    }
+}
