e1d08f083923214b8e4a0b009aae662e7537e70b
Juanpa1130 <pabloguzman121989@gmail.com>
2024-09-26 19:34:49 -0600

    arreglo
    arreglo

--- FacturacionLabco/FacturacionLabco/obj/Debug/net8.0/FacturacionLabco.AssemblyInfo.cs
+++ FacturacionLabco/FacturacionLabco/obj/Debug/net8.0/FacturacionLabco.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturacionLabco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6a34fede4255589c41719a4d9f3f0c1f656a63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6da1626fe796c333d2ea00ea2999a9a6ba7c896")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturacionLabco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturacionLabco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
