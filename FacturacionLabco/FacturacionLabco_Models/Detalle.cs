9771be191f1fcd92b550e5885edd254251965018
Dylshub-c <dylanperira0204@gmail.com>
2024-10-30 10:53:45 -0600

    jajajaja
    sdashdgaisydga

--- FacturacionLabco/FacturacionLabco_Models/Detalle.cs
+++ FacturacionLabco/FacturacionLabco_Models/Detalle.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FacturacionLabco_Models
 {
-    internal class Detalle
+    public class Detalle
     {
+        [Key]
+        public int Id { get; set; }
+
+        public double Monto { get; set; }
+
+        [Required(ErrorMessage = "Total de la factura es obligatorio")]
+        public int Cantidad { get; set; }
+
+        //Foreing key
+
+        public int ProductoID { get; set; }
+
+        [ForeignKey("ProductoID")]
+        public virtual Producto? Producto { get; set; }
+
+        public int ServicioID { get; set; }
+
+        [ForeignKey("ServicioID")]
+        public virtual Servicio? Servicio { get; set; }
     }
 }
