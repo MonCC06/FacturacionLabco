b1c3ef27e68fefc2d0dee2e9db445f9548076bab
Juanpa1130 <pabloguzman121989@gmail.com>
2024-11-01 09:36:07 -0600

    revision de models y  asjdnakjbdakjsdb
    asdasjdhaksjdbaksd

--- FacturacionLabco/FacturacionLabco_Models/Producto.cs
+++ FacturacionLabco/FacturacionLabco_Models/Producto.cs
@@ -18,5 +18,7 @@ namespace FacturacionLabco_Models
         [Required(ErrorMessage = "Stock del Producto es obligatorio")]
         [Range(1, int.MaxValue, ErrorMessage = "el Stock tiene que ser mayor a cero")]
         public float Stock_Producto { get; set; }
+
+        public int tipo {  get; set; }
     }
 }
