ee3b38a0fd5aec01d866c703ea31e7b92443e6d9
Dylshub-c <dylanperira0204@gmail.com>
2024-11-18 12:38:43 -0600

    prueba
    kskksa

--- FacturacionLabco/FacturacionLabco_AccesoDatos/obj/Debug/net8.0/FacturacionLabco_AccesoDatos.AssemblyInfo.cs
+++ FacturacionLabco/FacturacionLabco_AccesoDatos/obj/Debug/net8.0/FacturacionLabco_AccesoDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturacionLabco_AccesoDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b660aab31a9dfe7b4c7e8946c070da546ed1aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e341f2ed16b0e8e32968473eda7b32edf1028e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturacionLabco_AccesoDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturacionLabco_AccesoDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
