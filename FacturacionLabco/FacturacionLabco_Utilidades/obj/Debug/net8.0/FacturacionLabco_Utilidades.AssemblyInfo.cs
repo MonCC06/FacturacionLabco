7f29624dec911b36a607a5d44374d67959221771
Juanpa1130 <pabloguzman121989@gmail.com>
2024-11-24 16:08:24 -0600

    arreglo eliminar
    asdasdas

--- FacturacionLabco/FacturacionLabco_Utilidades/obj/Debug/net8.0/FacturacionLabco_Utilidades.AssemblyInfo.cs
+++ FacturacionLabco/FacturacionLabco_Utilidades/obj/Debug/net8.0/FacturacionLabco_Utilidades.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturacionLabco_Utilidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e93c8d0024e6d7cf882776c643bdde747d7bad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e02fb0ebbd4ecfcd03746f3737e0b663e4305bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturacionLabco_Utilidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturacionLabco_Utilidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
