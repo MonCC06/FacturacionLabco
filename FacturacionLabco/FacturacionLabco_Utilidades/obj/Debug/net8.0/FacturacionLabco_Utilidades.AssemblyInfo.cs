58bb5478d291f4cfb4c88d1d42535b4fa029fb10
Chepe <jdastorgamo1782@gmail.com>
2024-11-26 21:12:14 -0600

    NOTAASSIIMPRIMEE
    Opaaa

--- FacturacionLabco/FacturacionLabco_Utilidades/obj/Debug/net8.0/FacturacionLabco_Utilidades.AssemblyInfo.cs
+++ FacturacionLabco/FacturacionLabco_Utilidades/obj/Debug/net8.0/FacturacionLabco_Utilidades.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturacionLabco_Utilidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed50647ac95f2432201858447b7174ccc0632d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c118cd76e3793f518829f8f0632f3ce493811bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturacionLabco_Utilidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturacionLabco_Utilidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
