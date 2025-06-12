1a65b0de0372a84885944c1e8bbf719026ec001d
Juanpa <pabloguzman121989@gmail.com>
2025-06-12 01:01:15 -0600

    hb
    hb

--- FacturacionLabco/FacturacionLabco/obj/Debug/net8.0/FacturacionLabco.AssemblyInfo.cs
+++ FacturacionLabco/FacturacionLabco/obj/Debug/net8.0/FacturacionLabco.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturacionLabco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a652e595b7a7ecd53d98fc5dbd98f6e0e1656ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8626befeda21b6feb13b885631ded75a4246ac5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturacionLabco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturacionLabco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FacturacionLabco/FacturacionLabco/wwwroot/js/Factura.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('#FormFactura').on('submit', function (e) {
+        e.preventDefault(); // Evita el submit normal
+
+        var formData = $(this).serialize(); // Convierte los inputs en una cadena de consulta
+
+        $.ajax({
+            url: '/Factura/Upsert',
+            type: 'POST',
+            data: formData,
+            success: function (response) {
+                console.log("Respuesta del servidor:", response);
+                alert("Factura guardada con éxito.");
+                // Puedes redirigir o limpiar el formulario aquí
+            },
+            error: function (xhr) {
+                console.error("Error al guardar:", xhr.responseText);
+                alert("Ocurrió un error al guardar la factura.");
+            }
+        });
+    });
+});
